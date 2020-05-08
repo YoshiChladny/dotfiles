b7ce01998e9b8c4bf1ef86d40fd4b75eae2b1171
chla0002 <31961513+YoshiChladny@users.noreply.github.com>
2020-05-08 14:05:51 -0400

    update eslint global plugins

--- node/global.sh
+++ node/global.sh
@@ -1,9 +1,12 @@
 #list of npm packages to install globally
 npm install -g \
     eslint \
+    eslint-config-airbnb \ 
+    eslint-plugin-import \
+    eslint-plugin-jsx-a11y \
+    eslint-plugin-react \
     prettier \
     gatsby-cli \  
     nodemon \
     tslint \
     typescript \
-    uglify-js
